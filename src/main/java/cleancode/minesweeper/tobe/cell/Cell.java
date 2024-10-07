dbc81165b19b99cfaa9a834ad1d47cb93d7963c0
JeongBin0462 <dlwjdqls783@gmail.com>
2024-10-07 21:45:22 +0900

    상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,31 +1,14 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
-    protected static final String FLAG_SIGN = "⚑";
-    protected static final String UNCHECKED_SIGN = "□";
+public interface Cell {
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
 
-    protected boolean isFlagged;
-    protected boolean isOpened;
-
-    public abstract boolean isLandMine();
-
-    public abstract boolean hasLandMineCount();
-
-    public abstract String getSign();
-
-    public void flag() {
-        this.isFlagged = true;
-    }
-
-    public void open() {
-        this.isOpened = true;
-    }
-
-    public boolean isOpened() {
-        return isOpened;
-    }
-
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
+    boolean isLandMine();
+    boolean hasLandMineCount();
+    String getSign();
+    void flag();
+    void open();
+    boolean isOpened();
+    boolean isChecked();
 }
