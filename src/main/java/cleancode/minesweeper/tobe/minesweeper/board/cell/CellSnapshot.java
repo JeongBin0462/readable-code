0a00b3ac0fa5d3fac6abca0fd0bb566f682839d0
JeongBin0462 <dlwjdqls783@gmail.com>
2024-10-09 14:17:38 +0900

    패캐지 정리

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellSnapshot.java
@@ -0,0 +1,44 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public class CellSnapshot {
+
+    private final CellSnapshotStatus status;
+    private final int nearbyLandMineCount;
+
+    private CellSnapshot(CellSnapshotStatus status, int landMineCount) {
+        this.status = status;
+        this.nearbyLandMineCount = landMineCount;
+    }
+
+    public boolean isSameStatus(CellSnapshotStatus cellSnapshotStatus) {
+        return this.status == cellSnapshotStatus;
+    }
+
+    public static CellSnapshot ofEmpty() {
+        return new CellSnapshot(CellSnapshotStatus.EMPTY, 0);
+    }
+
+    public static CellSnapshot ofFlag() {
+        return new CellSnapshot(CellSnapshotStatus.FLAG, 0);
+    }
+
+    public static CellSnapshot ofLandMine() {
+        return new CellSnapshot(CellSnapshotStatus.LAND_MINE, 0);
+    }
+
+    public static CellSnapshot ofNumber(int nearbyLandMineCount) {
+        return new CellSnapshot(CellSnapshotStatus.NUMBER, nearbyLandMineCount);
+    }
+
+    public static CellSnapshot ofUnchecked() {
+        return new CellSnapshot(CellSnapshotStatus.UNCHECKED, 0);
+    }
+
+    public CellSnapshotStatus getStatus() {
+        return status;
+    }
+
+    public int getNearbyLandMineCount() {
+        return nearbyLandMineCount;
+    }
+}
