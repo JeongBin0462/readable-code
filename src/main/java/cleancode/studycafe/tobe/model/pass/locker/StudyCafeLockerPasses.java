fd9c689eba29aa8e380cab51eb4616129935d9f2
JeongBin0462 <dlwjdqls783@gmail.com>
2024-10-13 16:59:10 +0900

    dsiplay() 로직 이관

--- src/main/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPasses.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPasses.java
@@ -1,11 +1,13 @@
-package cleancode.studycafe.tobe.model;
+package cleancode.studycafe.tobe.model.pass.locker;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
 
 import java.util.List;
 import java.util.Optional;
 
 public class StudyCafeLockerPasses {
 
-    public final List<StudyCafeLockerPass> lockerPasses;
+    private final List<StudyCafeLockerPass> lockerPasses;
 
     private StudyCafeLockerPasses(List<StudyCafeLockerPass> lockerPasses) {
         this.lockerPasses = lockerPasses;
@@ -15,9 +17,10 @@ public class StudyCafeLockerPasses {
         return new StudyCafeLockerPasses(lockerPasses);
     }
 
-    public Optional<StudyCafeLockerPass> findLockerPassBy(StudyCafePass pass) {
+    public Optional<StudyCafeLockerPass> findLockerPassBy(StudyCafeSeatPass pass) {
         return lockerPasses.stream()
             .filter(pass::isSameDurationType)
             .findFirst();
     }
+
 }
